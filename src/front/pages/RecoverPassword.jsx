5d67fdee0af817a8afb515b00bb507c964a770a6
Mariabel17 <maribelml17@gmail.com>
2026-03-03 15:01:43 +0000

    work

---
+++ src/front/pages/RecoverPassword.jsx
@@ -0,0 +1,68 @@
+import { useState } from "react";
+
+
+const BACKEND_URL = import.meta.env.VITE_BACKEND_URL
+
+export const RecoverPassword = () => {
+
+    const [email, setEmail] = useState("")
+    const [message, setMessage] = useState("")
+    const [loading, setLoading] = useState(false)
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        setLoading(true)
+        setMessage("")
+
+
+        try {
+            const response = await fetch (`${BACKEND_URL}/api/forgot-password`,{
+                method: "POST",
+                headers: {"Content-Type": "aplication/json"},
+                body: JSON.stringify({
+                    token,
+                    new_password: password
+                })
+        })
+
+            const data = await response.json()
+
+            setMessage(data.msg)
+
+        }   catch (error) {
+            setMessage("Error con la solicitud")
+        }
+
+        setLoading(false)
+        
+    }
+
+    
+
+    return (
+        <div className="container d-flex justify-content-center flex-column mt-4">
+            
+            
+            <div className="border container rounded-4 py-4 shadow" style={{ width: "450px" }}>
+                <h2 className="text-center">Recuperar Contraseña</h2>
+            
+                <form onSubmit={handleSubmit}>
+                    <input
+                    type="email"
+                    placeholder= "introduce tu email"
+                    value={email}
+                    onChange={(e)=>( setEmail(e.target.value))}
+                    required
+                    />
+
+                    <button type="submit" disabled={loading}>
+                        {loading ? "Enviando..." : "Enviar"}
+                    </button>
+                </form>
+
+                {message && <p>{message}</p>}
+            </div>
+
+        </div>
+    )
+}
